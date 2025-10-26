e9a7eec28598fd8991f7662b244f712b85dd5023
walid666 <12078@holbertonstudents.com>
2025-10-26 14:09:30 +0000

    Add 1-last_digit.c script

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,27 +3,28 @@
 #include <stdio.h>
 
 /**
- * main - affiche le dernier chiffre d'un nombre
- * Return: Always 0 (Succès)
+ * main - prints the last digit of a random number
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int n;
-    int last_digit;
+	int n;
+	int last_digit;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-    last_digit = n % 10;
+	last_digit = n % 10;
 
-    printf("Last digit of %d is %d ", n, last_digit);
+	printf("Last digit of %d is %d ", n, last_digit);
 
-    if (last_digit > 5)
-        printf("and is greater than 5\n");
-    else if (last_digit == 0)
-        printf("and is 0\n");
-    else
-        printf("and is less than 6 and not 0\n");
+	if (last_digit > 5)
+		printf("and is greater than 5\n");
+	else if (last_digit == 0)
+		printf("and is 0\n");
+	else
+		printf("and is less than 6 and not 0\n");
 
-    return (0);
+	return (0);
 }
