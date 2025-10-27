d28efedc72038aa3d6f3ab678c9fa10da5e7d926
walid666-ai <12078@holbertonstudents.com>
2025-10-27 15:54:50 +0000

    Add 9-times_table.c and update main.h

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int i, j, prod;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			prod = i * j;
+
+			if (j == 0)
+			{
+				_putchar(prod + '0');
+			}
+			else if (prod < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(prod + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
